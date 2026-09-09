1188b1e78303f55052d9a5a92763f886238b6b01
Gerally Medina Rivera <gerally.medinarivera99@login.cuny.edu>
2026-09-09 17:38:03 -0400

    Added if statement for no height and comments to HW1

--- HW1_MedinaRivera_Gerally.py
+++ HW1_MedinaRivera_Gerally.py
@@ -5,9 +5,12 @@ Homework 1 for Computational Astrophysics
 ASTR 178100
 Prof. A. Maller
 """
+# Imports
 
 import sys
 import argparse
+
+# Functions
 
 def time_drop(height:float, gravity:float):
     """
@@ -26,8 +29,11 @@ def time_drop(height:float, gravity:float):
 
     return time
 
+# Main Code
+
 if __name__ == '__main__':
 
+    # argparse to enter values from terminal
     parser = argparse.ArgumentParser()
     parser.add_argument('-height', type=float, help='Height of ball above ground.')
     parser.add_argument('-u', type=str, default='m', help='Units of the height. Default is meters ("m"), '
@@ -38,7 +44,11 @@ if __name__ == '__main__':
                                                                                 'Default is Earths gravity (9.8).')
     args = parser.parse_args()
 
+    # in case no height is given
+    if args.height == None:
+        sys.exit('No value for height entered. Use "-h" or "-help" to view how to provide a value.')
 
+    # converting any value to meters
     if args.u == 'm':
         user_height_meters = args.height
         units = 'meters'
@@ -55,10 +65,11 @@ if __name__ == '__main__':
         user_height_meters = args.height/39.37
         units = 'inches'
 
-    else:
+    else:    # in case invalid units were given
         sys.exit('The units entered are invalid. Use "-h" or "-help" to view which units can be used.')
-
 
     result = time_drop(user_height_meters, args.gravity)
 
     print(f'The time it takes a ball dropped from {args.height} {units} to reach the ground is {result:.2f} seconds.')
+
+# End of Code
